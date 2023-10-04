72d0e6881b757a59f72e0b51fc9d7167da4fd41d
Eightina <orion.que@outlook.com>
2023-10-03 23:59:30 -0400

    temp update

--- project1/src/scripts/sbatch_PartB.sh
+++ project1/src/scripts/sbatch_PartB.sh
@@ -24,15 +24,15 @@ echo "Current directory: ${CURRENT_DIR}"
 # srun -n 1 --cpus-per-task 1 ${CURRENT_DIR}/../../build/src/cpu/simd_PartB ${CURRENT_DIR}/../../images/20K-RGB.jpg ${CURRENT_DIR}/../../images/20K-Smooth.jpg
 # echo ""
 
-MPI PartB
-echo "MPI PartB (Optimized with -O2)"
-for num_processes in 16
-# 1 2 4 8 16 32
-do
-  echo "Number of processes: $num_processes"
-  srun -n $num_processes --cpus-per-task 1 --mpi=pmi2 ${CURRENT_DIR}/../../build/src/cpu/mpi_PartB ${CURRENT_DIR}/../../images/20K-RGB.jpg ${CURRENT_DIR}/../../images/20K-Smooth.jpg
-  echo ""
-done
+# MPI PartB
+# echo "MPI PartB (Optimized with -O2)"
+# for num_processes in 16
+# # 1 2 4 8 16 32
+# do
+#   echo "Number of processes: $num_processes"
+#   srun -n $num_processes --cpus-per-task 1 --mpi=pmi2 ${CURRENT_DIR}/../../build/src/cpu/mpi_PartB ${CURRENT_DIR}/../../images/20K-RGB.jpg ${CURRENT_DIR}/../../images/20K-Smooth.jpg
+#   echo ""
+# done
 
 # # Pthread PartB
 # echo "Pthread PartB (Optimized with -O2)"
@@ -43,14 +43,15 @@ done
 #   echo ""
 # done
 
-# # OpenMP PartB
-# echo "OpenMP PartB (Optimized with -O2)"
-# for num_cores in 1 2 4 8 16 32
-# do
-#   echo "Number of cores: $num_cores"
-#   srun -n 1 --cpus-per-task $num_cores ${CURRENT_DIR}/../../build/src/cpu/openmp_PartB ${CURRENT_DIR}/../../images/20K-RGB.jpg ${CURRENT_DIR}/../../images/20K-Smooth.jpg ${num_cores}
-#   echo ""
-# done
+# OpenMP PartB
+echo "OpenMP PartB (Optimized with -O2)"
+for num_cores in 1 2 
+# 4 8 16 32
+do
+  echo "Number of cores: $num_cores"
+  srun -n 1 --cpus-per-task $num_cores ${CURRENT_DIR}/../../build/src/cpu/openmp_PartB ${CURRENT_DIR}/../../images/20K-RGB.jpg ${CURRENT_DIR}/../../images/20K-Smooth.jpg ${num_cores}
+  echo ""
+done
 
 # # CUDA PartB
 # echo "CUDA PartB"
