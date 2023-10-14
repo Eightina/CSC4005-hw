62f9e20fa85df51c6d9678ecad16b0bbc265e216
Eightina <orion.que@outlook.com>
2023-10-14 06:11:13 -0400

    Proj.2: fixing preload bug, improving

--- project2/src/locality.cpp
+++ project2/src/locality.cpp
@@ -59,28 +59,48 @@ void inline kij_tmm(int M, int N, int K, const Matrix& matrix1, const Matrix& ma
     }
 }
 
+void inline preload_block(void *__restrict dst, const void *__restrict src, int block_size) {
+    #pragma GCC unroll 64
+    for (int i = 0; i < block_size; ++i) {
+        memcpy(dst, src, block_size);
+        dst += block_size;
+    }
+}
+
 void inline ijk_kij_tmm(int M, int N, int K, const Matrix& matrix1, const Matrix& matrix2, Matrix& result, int block_size) {
     for (int i = 0; i < M; i+=block_size) {
         for (int j = 0; j < N; j+=block_size) {
+            // int zeroload_result[block_size * block_size];
+            // preload_block(zeroload_result, result[i]+j, block_size);
             for (int k = 0; k < K; k+=block_size) {
                 
-                // int preload_r1[] = 
+                // int zeroload_matrix1[block_size * block_size];
+                // preload_block(zeroload_matrix1, matrix1[i]+k, block_size);
+                // int zeroload_matrix2[block_size * block_size];
+                // preload_block(zeroload_matrix2, matrix2[k]+j, block_size);
 
+                //------------------kernel----------------------------
                 // #pragma GCC unroll 64
                 for (int k1 = k; k1 < k+block_size; ++k1) {
                     // #pragma GCC unroll 64
                     register int preload_matrix2[block_size];
                     memcpy(preload_matrix2, matrix2[k1], block_size);
+
                     for (int i1 = i; i1 < i+block_size; ++i1) {
+
                         register int r1 = matrix1[i1][k1];
-                        register int preload_result[block_size];
-                        memcpy(preload_result, result[i1], block_size);
+
+                        // register int preload_result[block_size];
+                        // memcpy(preload_result, result[i1], block_size);
                         // #pragma GCC unroll 64
                         for (int j1 = j; j1 < j+block_size; ++j1) {
-                            result[i1][j1] = preload_result[j1-j] + r1 * preload_matrix2[j1-j];  
+                            // result[i1][j1] = preload_result[j1-j] + r1 * preload_matrix2[j1-j];  
+                            result[i1][j1] += r1 * preload_matrix2[j1-j];  
                         }
+
                     }
                 }
+                //------------------kernel----------------------------
 
             }
         }
@@ -112,7 +132,7 @@ Matrix matrix_multiply_locality(const Matrix& matrix1, const Matrix& matrix2) {
     // kij_tmm(M, N, K, matrix1, matrix2, result, 4);       // kij 1024*1024:5275ms; 
 
     // ijk_kij_tmm(M, N, K, matrix1, matrix2, result, 32);     // ijk_kij 1024:3693ms; 
-    ijk_kij_tmm(M, N, K, matrix1, matrix2, result, 64);     // ijk_kij 1024:3674ms; preload1024:2394ms;
+    ijk_kij_tmm(M, N, K, matrix1, matrix2, result, 64);     // ijk_kij 1024:3674ms; preload1024:1983ms;
     // ijk_kij_tmm(M, N, K, matrix1, matrix2, result, 128);    // ijk_kij 1024:4571ms; 
 
     // Your Code Here!
